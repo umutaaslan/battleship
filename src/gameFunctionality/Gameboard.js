9cc46873de20e88f2533425ba8ab3048db74cd1a
umutaaslan <umutalperaslan705@hotmail.com>
2025-06-07 14:15:35 +0300

    structure grid[x][y] and add placeShip and isPlaceable functions

--- src/gameFunctionality/Gameboard.js
+++ src/gameFunctionality/Gameboard.js
@@ -1,17 +1,45 @@
-export default class Gameboard{
-    constructor(){
-        this.grid = Array.from({length:10}, () => Array.from({length: 10}, () => null));
-        // for(let i = 0; i < 10; i++){
-        //     this.grid[i] = [];
-        //     for(let j = 0; j < 10; j++){
-        //         this.grid[i].push(null);
-        //     }
-        // }
-    }
-    
-    placeShip(ship, x, y, axis){
+export default class Gameboard {
+	constructor() {
+		this.grid = Array.from({ length: 10 }, () =>
+			Array.from({ length: 10 }, () => null)
+		);
 
-    }
+		// for(let i = 0; i < 10; i++){
+		//     this.grid[i] = [];
+		//     for(let j = 0; j < 10; j++){
+		//         this.grid[i].push(null);
+		//     }
+		// }
+	}
+
+	placeShip(ship, x, y, axis) {
+		if (this.isShipPlaceable(ship, x, y, axis)) {
+			if (axis === "x") {
+				for (let i = x; i < x + ship.length; i++) {
+					if (Array.isArray(this.grid[i]) && this.grid[i][y] === null) {
+						this.grid[i][y] = ship.name;
+					}
+				}
+			}
+
+			return true;
+		}
+
+		return false;
+	}
+
+	isShipPlaceable(ship, x, y, axis) {
+		if (x >= 0 && x < 10 && y >= 0 && y < 10) {
+			if (axis === "x") {
+				for (let i = x; i < x + ship.length; i++) {
+					if (!Array.isArray(this.grid[i]) || this.grid[i][y] !== null) {
+						return false;
+					}
+				}
+				return true;
+			}
+		} else {
+			return false;
+		}
+	}
 }
-
-console.log((new Gameboard()).grid)
--- src/gameFunctionality/tests/Gameboard.test.js
+++ src/gameFunctionality/tests/Gameboard.test.js
@@ -12,7 +12,7 @@ test("gameboard is 10x10", () => {
 
 test("places ships on x axis properly", () => {
 	const gameboard = new Gameboard();
-	gameboard.placeShip(new Ship("ship1", 5), 2, 1, "x");
+	expect(gameboard.placeShip(new Ship("ship1", 5), 2, 1, "x")).toBe(true);	
     expect(gameboard.grid[2][1]).toBe("ship1");
     expect(gameboard.grid[3][1]).toBe("ship1");
     expect(gameboard.grid[4][1]).toBe("ship1");
@@ -20,8 +20,23 @@ test("places ships on x axis properly", () => {
     expect(gameboard.grid[6][1]).toBe("ship1");
 });
 
+test("isShipPlaceable works", () => {
+	const gameboard = new Gameboard();
+	expect(gameboard.isShipPlaceable(new Ship("ship1", 5), 7, 1, "x")).toBe(false);
+	expect(gameboard.isShipPlaceable(new Ship("ship1", 5), 7, 0, "x")).toBe(false);
+	expect(gameboard.isShipPlaceable(new Ship("ship1", 5), 5, 0, "x")).toBe(true);
+})
+
+
 test("throw error when ship is off grid", () => {
 	const gameboard = new Gameboard();
 
-	expect(gameboard.placeShip(new Ship("ship1", 5), 7, 1, "x")).toBe("off grid");
+	expect(gameboard.placeShip(new Ship("ship1", 5), 12, 0, "x")).toBe(false);
+	expect(gameboard.placeShip(new Ship("ship1", 5), 4, -2, "x")).toBe(false);
+	expect(gameboard.placeShip(new Ship("ship1", 5), 4, 14, "x")).toBe(false);
+	expect(gameboard.placeShip(new Ship("ship1", 5), 7, 1, "x")).toBe(false);
+	gameboard.placeShip(new Ship("ship1", 5), 7, 1, "x")
+	expect(gameboard.grid[7][1]).toBe(null);
+	expect(gameboard.grid[8][1]).toBe(null);
+	expect(gameboard.grid[9][1]).toBe(null);
 });
